ab190ed4f4e0c22ae91a338dce2e6550973394ad
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-12-14 10:50:38 -0500

    PrivateRoute retornada ao modelo antigo

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,9 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-function PrivateRoute({ children }) {
+const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
-  const location = useLocation();
-
-  if (!isAuthenticated) {
-    // Redireciona para login, mas salva a rota original no estado
-    return <Navigate to="/login" state={{ from: location }} />;
-  }
-
-  return children;
-}
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
 export default PrivateRoute;
